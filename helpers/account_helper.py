980ff8bbd492a6e88706ab90cc72002d43d2f561
Yakushenko Anton Aleksandrovich <yakushenko.a.a@sblogistica.ru>
2025-04-15 18:15:14 +0400

    fix

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -20,20 +20,18 @@ class AccountHelper:
             password: str,
             email: str
     ):
+
         json_data = {
             'login': login,
             'email': email,
             'password': password,
         }
-
         response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
-        assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
-        response = self.mailhog.mailhog_api.get_api_v2_messages()
-        assert response.status_code == 200, "Письма не были получены"
-        token = self.get_activation_token_by_login(login=login, response=response)
+        assert response.status_code == 201, "Пользователь не был создан"
+
+        token = self.get_activation_token_by_login(login=login)
         assert token is not None, f"Токен для пользователя {login} не был получен"
-        response = self.dm_account_api.account_api.put_v1_account_token(token=token)
-        assert response.status_code == 200, "Пользователь не был активирован"
+        response = self.activation_token(token=token)
         return response
 
     def user_login(
@@ -66,39 +64,12 @@ class AccountHelper:
         response = self.dm_account_api.account_api.put_v1_account_email(json_data=change_email_data)
         assert response.status_code == 200, f"Email не был обновлен: {response.text}"
 
-    def activate_token(
+    def get_activation_token_by_login(
             self,
-            login: str,
-            ):
+            login
+    ):
         response = self.mailhog.mailhog_api.get_api_v2_messages()
         assert response.status_code == 200, "Письма не были получены"
-        token = self.get_activation_token_by_login(login=login, response=response)
-        assert token is not None, f"Токен для пользователя {login} не был получен"
-        response = self.dm_account_api.account_api.put_v1_account_token(token=token)
-        assert response.status_code == 200, "Пользователь не был активирован"
-        return response
-
-    def forbidden_login(
-            self,
-            login: str,
-            password: str,
-            remember_me: bool = True
-    ):
-        json_data = {
-            'login': login,
-            'password': password,
-            'rememberMe': remember_me,
-        }
-
-        response = self.dm_account_api.login_api.post_v1_account_login(json_data=json_data)
-        assert response.status_code == 403, "Login with old email should be forbidden"
-        return response
-
-    @staticmethod
-    def get_activation_token_by_login(
-            login,
-            response
-    ):
         token = None
         for item in response.json()['items']:
             try:
@@ -110,3 +81,11 @@ class AccountHelper:
                 token = user_data['ConfirmationLinkUrl'].split('/')[-1]
                 print(token)
         return token
+
+    def activation_token(
+            self,
+            token
+    ):
+        response = self.dm_account_api.account_api.put_v1_account_token(token=token)
+        assert response.status_code == 200, "Пользователь не был активирован"
+        return response
--- tests/functional/put_v1_account_email/test_put_v1_account_email.py
+++ tests/functional/put_v1_account_email/test_put_v1_account_email.py
@@ -29,14 +29,24 @@ def test_change_email():
     login = f'henry3{data}'
     email = f'{login}@mail.ru'
     password = '123456789'
-    account_helper.register_new_user(login=login,password=password, email=email)
-    account_helper.user_login(login=login, password=password)
     new_email = f'new{int(time.time())}@test.com'
-    account_helper.change_email(login=login,password=password,new_email=new_email)
-    # Попытка входа со старым email (должен быть 403)
-    account_helper.forbidden_login(login=login,password=password)
-    # Получение токена подтверждения нового email
-    # Получить активационный токен
-    # Активация нового email
-    account_helper.activate_token(login=login)
+    account_helper.register_new_user(login=login, password=password, email=email)
+    account_helper.user_login(login=login, password=password)
+    account_helper.change_email(login=login, password=password, new_email=new_email)
+
+    # Пытаемся войти, получаем 403
+    json_data = {
+        'login': login,
+        'password': password,
+        'remember_me': True,
+    }
+    response = account_helper.dm_account_api.login_api.post_v1_account_login(json_data=json_data)
+    assert response.status_code == 403, "Пользователь авторизован"
+
+
+    token = account_helper.get_activation_token_by_login(login=login)
+    assert token is not None, f"Токен для пользователя {login} не был получен"
+    # Активируем этот токен
+    account_helper.activation_token(token=token)
+    # Логинимся
     account_helper.user_login(login=login, password=password)
