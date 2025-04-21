a147a02724556a01d1c02bffa19661a2e8ca9187
Yakushenko Anton Aleksandrovich <yakushenko.a.a@sblogistica.ru>
2025-04-21 17:35:25 +0400

    add asserts

--- helpers/account_helper.py
+++ helpers/account_helper.py
@@ -48,12 +48,8 @@ class AccountHelper:
             login: str,
             password: str,
     ):
-        response = self.dm_account_api.login_api.post_v1_account_login(
-            json_data={
-                "login": login,
-                "password": password
-            }
-        )
+        response = self.user_login(login=login, password= password)
+
         token = {
             "x-dm-auth-token": response.headers["x-dm-auth-token"]
         }
@@ -72,9 +68,11 @@ class AccountHelper:
             'password': password,
         }
         response = self.dm_account_api.account_api.post_v1_account(json_data=json_data)
-        assert response.status_code == 201, "Пользователь не был создан"
-
+        assert response.status_code == 201, f"Пользователь не был создан {response.json()}"
+        start_time = time.time()
         token = self.get_activation_token_by_login(login=login)
+        end_time = time.time()
+        assert end_time - start_time < 0.1, "Время ожидания активации превышено"
         assert token is not None, f"Токен для пользователя {login} не был получен"
         response = self.activation_token(token=token)
         return response
@@ -104,6 +102,7 @@ class AccountHelper:
         }
 
         response = self.dm_account_api.login_api.post_v1_account_login(json_data=json_data)
+        assert response.headers["x-dm-auth-token"], "Токен для пользователя не был получен"
         assert response.status_code == 200, "Пользователь не смог авторизоваться"
         return response
 
