f83cf134243ef3d2b9190edd40db2a436dfb28b7
Yakushenko Anton Aleksandrovich <yakushenko.a.a@sblogistica.ru>
2025-04-24 12:54:05 +0400

    add context manager

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from checkers.http_ckeckers import check_status_code_http
 from hamcrest import (
     assert_that,
     has_property,
@@ -9,20 +9,21 @@ from hamcrest import (
 )
 
 
+
 def test_get_v1_account_auth(
         auth_account_helper
 ):
-    response = auth_account_helper.dm_account_api.account_api.get_v1_account()
-    assert_that(
-        response, all_of(
-            has_property('resource', has_property('login', starts_with("mystery"))),
-            has_property('resource', has_property('registration', instance_of(datetime)))
+        response = auth_account_helper.dm_account_api.account_api.get_v1_account()
+        assert_that(
+            response, all_of(
+                has_property('resource', has_property('login', starts_with("mystery"))),
+                has_property('resource', has_property('registration', instance_of(datetime)))
+            )
         )
-    )
 
 
 def test_get_v1_account_no_auth(
         account_helper
 ):
-    resp = account_helper.dm_account_api.account_api.get_v1_account(validate_response=False)
-    assert resp.status_code == 401, f"Ожидали 401 Unauthorized, получили {resp.status_code}"
+    with check_status_code_http(401, 'User must be authenticated'):
+        account_helper.dm_account_api.account_api.get_v1_account(validate_response=False)
