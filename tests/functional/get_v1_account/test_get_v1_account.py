9902a2fbf0a7a216dc457f7ed40b56153ba70a2e
Yakushenko Anton Aleksandrovich <yakushenko.a.a@sblogistica.ru>
2025-04-26 15:49:07 +0400

    add checker_functions

--- tests/functional/get_v1_account/test_get_v1_account.py
+++ tests/functional/get_v1_account/test_get_v1_account.py
@@ -1,22 +1,13 @@
-from datetime import datetime
-
-from assertpy import (
-    soft_assertions,
-    assert_that,
-)
-
+from checkers.get_v1_account import GetV1Account
 from checkers.http_ckeckers import check_status_code_http
-from dm_api_account.models.user_details_envelope import UserRole
+
 
 
 def test_get_v1_account_auth(
         auth_account_helper
 ):
         response = auth_account_helper.dm_account_api.account_api.get_v1_account()
-        with soft_assertions():
-            assert_that(response.resource.login).is_equal_to("mystery15")
-            assert_that(response.resource.online).is_instance_of(datetime)
-            assert_that(response.resource.roles).contains(UserRole.guest, UserRole.player)
+        GetV1Account.check_response_values(response)
 
 
 def test_get_v1_account_no_auth(
--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -1,16 +1,6 @@
-from datetime import datetime
 import pytest
 from checkers.http_ckeckers import check_status_code_http
-from hamcrest import (
-    assert_that,
-    has_property,
-    starts_with,
-    all_of,
-    instance_of,
-    has_properties,
-    equal_to,
-)
-
+from checkers.post_v1_account import PostV1Account
 
 
 def test_post_v1_account(
@@ -22,26 +12,9 @@ def test_post_v1_account(
     email = prepare_user.email
     account_helper.register_new_user(login=login, password=password, email=email)
     response = account_helper.user_login(login=login, password=password, validate_response=True)
-    assert_that(
-        response, all_of(
-             has_property('resource', has_property('login', starts_with("henry"))),
-             has_property('resource', has_property('registration', instance_of(datetime))),
-             has_property(
-                'resource', has_properties(
-                    {
-                        'rating': has_properties(
+    PostV1Account.check_response_values(response)
 
-                            {
-                                "enabled": equal_to(True),
-                                "quality": equal_to(0),
-                                "quantity": equal_to(0)
-                            }
-                        )
-                    }
-                )
-            )
-        )
-    )
+
 
 
 
